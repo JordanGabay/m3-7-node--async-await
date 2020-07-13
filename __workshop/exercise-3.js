f794f9660c537bd97992fbbdcd9bc0ddea822c17
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-13 15:54:10 -0400

    Exercise 3 Complete

--- __workshop/exercise-3.js
+++ __workshop/exercise-3.js
@@ -3,10 +3,27 @@
 
 const doublesLater = (num) => {
   // 1. waits 2 seconds (You could console a message here. It might make the 2 second wait easier ;)
-  // 2. returns the double of the num
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(num * 2)
+    }, 2000)
+  });
 };
+  
 
-// 3. handleSum function (async/await)
+// 2. handleSum function (async/await)
+
+const handleSum = async (num) => {
+  try {
+    const times2 = await doublesLater(num);
+    const times3 = await doublesLater(num);
+    const times4 = await doublesLater(num);
+    return times2 + times3 + times4
+  } 
+    catch (err) {
+    console.log('Error: ', err);
+  }
+}
 
 // 4. verification
 handleSum(10).then((ans) => console.log(ans));
