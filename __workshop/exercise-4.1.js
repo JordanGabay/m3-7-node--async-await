39e85e5d403a21a3f75aaa2e860e0b07781cbcdc
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-13 16:07:45 -0400

    4.1 Complete

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -3,4 +3,18 @@ const request = require('request-promise');
 // getDadJoke
 
 // 4.1
-// getJoke().then((data) => console.log(data));
+
+const getDadJoke = async () => {
+    try {
+        const response = await request( {
+            headers: {Accept: 'application/json'},
+            uri: "https://icanhazdadjoke.com/"
+        })
+        const dadJoke = JSON.parse(response);
+        return dadJoke.joke;
+    } catch (err) {
+        console.log('Error: ', err);
+    }
+};
+
+getDadJoke().then((data) => console.log(data));
